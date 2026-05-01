10d0f5ce7dc4ca349b7e862f3df276c590a6c6b2
Aminul islam <aminurrohman98@gmail.com>
2026-05-02 00:33:41 +0100

    refactor the proxy file

--- src/proxy.js
+++ src/proxy.js
@@ -17,5 +17,5 @@ export async function proxy(request) {
 // export default function proxy(request) { ... }
 
 export const config = {
-  matcher: ["/profile", "/allTiles/:id*"],
+  matcher: ["/profile", "/allTiles/:id"],
 };

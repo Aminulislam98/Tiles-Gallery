e4340101a53002d3710775b375106618c2238479
Aminul islam <aminurrohman98@gmail.com>
2026-05-03 21:34:07 +0100

    remove the href link from from

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -26,7 +26,7 @@ export default function LoginForm() {
       return;
     }
 
-    setTimeout(() => router.push(`${callbackUrl}?toast=welcome`), 500);
+    router.push(`${callbackUrl}?toast=welcome`);
   };
 
   const signInByGoogle = async () => {
@@ -250,7 +250,7 @@ export default function LoginForm() {
             </TextField>
             <div className="flex gap-2">
               <button
-                href={`/?callbackUrl=${callbackUrl}`}
+                // href={`/?callbackUrl=${callbackUrl}`}
                 type="submit"
                 className="bg-black w-full py-3 rounded-xl text-sm font-medium text-white hover:bg-[#B85C38] transition-all duration-200 flex flex-row items-center justify-center gap-2 "
               >
